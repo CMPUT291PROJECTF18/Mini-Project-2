c118ec639c6a9b3a1411a6ec0dd456de49f87a53
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-24 19:59:56 -0700

    Parser now works for pdates

--- mini_project_2/parser.py
+++ mini_project_2/parser.py
@@ -8,6 +8,8 @@ import xml.etree.ElementTree as ElementTree
 def parse(xml_string):
     term_string = parse_terms(xml_string)
     create_terms_file(term_string)
+    pdate_string = parse_pdates(xml_string)
+    create_pdates_file(pdate_string)
 
 
 def parse_terms(xml_string):
@@ -18,19 +20,19 @@ def parse_terms(xml_string):
     for ad in root.findall("ad"):
         aid = ad.find("aid").text
 
-        titles = ad.find("ti").text
-        titles = re.sub("['\"]", "", titles)
+        title = ad.find("ti").text
+        title = re.sub("['\"]", "", title)
 
-        for title in re.split("(?!-)\W", titles):
-            if len(title) > 2 and re.match("^[0-9a-zA-Z_\-]*$", title, 0):
-                terms.append(title.lower() + ":" + aid)
+        for token in re.split("(?!-)\W", title):
+            if len(token) > 2 and re.match("^[0-9a-zA-Z_\-]*$", token, 0):
+                terms.append(token.lower() + ":" + aid)
 
-        descriptions = ad.find("desc").text
-        descriptions = re.sub("['\"]", "", descriptions)
+        description = ad.find("desc").text
+        description = re.sub("['\"]", "", description)
 
-        for desc in re.split("(?!-)\W", descriptions):
-            if len(desc) > 2 and re.match("^[0-9a-zA-Z_\-]*$", desc, 0):
-                terms.append(desc.lower() + ":" + aid)
+        for token in re.split("(?!-)\W", description):
+            if len(token) > 2 and re.match("^[0-9a-zA-Z_\-]*$", token, 0):
+                terms.append(token.lower() + ":" + aid)
 
     term_string = ""
     for i, term in enumerate(terms):
@@ -44,5 +46,32 @@ def parse_terms(xml_string):
 
 def create_terms_file(term_string):
     # TODO: Implement
-    # TODO: Discussion: Create files in a different module (better cohesion)?
+    return
+
+
+def parse_pdates(xml_string):
+    pdates = list()
+
+    elem = ElementTree.fromstring(xml_string)
+    root = ElementTree.ElementTree(elem).getroot()
+
+    for ad in root.findall("ad"):
+        date = ad.find("date").text
+        aid = ad.find("aid").text
+        category = ad.find("cat").text
+        location = ad.find("loc").text
+        pdates.append(date + ":" + aid + "," + category + "," + location)
+
+    pdate_string = ""
+    for i, term in enumerate(pdates):
+        if i == len(pdates) - 1:
+            pdate_string += term
+        else:
+            pdate_string += (term + "\n")
+
+    return pdate_string
+
+
+def create_pdates_file(pdate_string):
+    # TODO: Implement
     return
--- test/unit/test_parser.py
+++ test/unit/test_parser.py
@@ -12,7 +12,7 @@ def test_parse_terms_10():
 
     term_string = parser.parse_terms(data_string)
 
-    with open("../data/10_expected.txt", "r") as expected:
+    with open("../data/terms_expected_10.txt", "r") as expected:
         expected_string = expected.read()
 
     assert term_string == expected_string
@@ -24,9 +24,31 @@ def test_parse_terms_1000():
 
     term_string = parser.parse_terms(data_string)
 
-    with open("../data/1000_expected.txt", "r") as expected:
+    with open("../data/terms_expected_1000.txt", "r") as expected:
         expected_string = expected.read()
 
     assert term_string == expected_string
 
 
+def test_parse_pdates_10():
+    with open("../data/10.xml", "r") as data:
+        data_string = data.read()
+
+    pdate_string = parser.parse_pdates(data_string)
+
+    with open("../data/pdates_expected_10.txt", "r") as expected:
+        expected_string = expected.read()
+
+    assert pdate_string == expected_string
+
+
+def test_parse_pdates_1000():
+    with open("../data/1000.xml", "r") as data:
+        data_string = data.read()
+
+    pdate_string = parser.parse_pdates(data_string)
+
+    with open("../data/pdates_expected_1000.txt", "r") as expected:
+        expected_string = expected.read()
+
+    assert pdate_string == expected_string
