56a100332b36ba7a4c3cb85497fe44dd2ea88cf9
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-25 13:39:36 -0700

    updating install script

--- mini_project_2/scripts/install.sh
+++ mini_project_2/scripts/install.sh
@@ -4,6 +4,7 @@ sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev -y
 sudo apt-get install db-util -y
+sudo add-apt-repository -r ppa:bitcoin/bitcoin -y
 
 mkdir ~/part2
 mv part2.sh ~/part2/
