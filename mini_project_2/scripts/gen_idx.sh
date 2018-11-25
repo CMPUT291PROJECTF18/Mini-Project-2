0302df26389403cec90b53bf358e4d3afae408f0
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-25 11:33:30 -0700

    adding some useful scripts for part2

---
+++ mini_project_2/scripts/gen_idx.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+~/./part2/part2.sh ads.txt hash ads.idx
+~/./part2/part2.sh terms.txt btree terms.idx
+~/./part2/part2.sh pdates.txt btree pdates.idx
+~/./part2/part2.sh prices.txt btree prices.idx
