ea1d8786957dddeb3f12eebdf287ce0b33319755
Thomas Lorincz <tlorincz@ualberta.ca>
2018-11-26 11:48:29 -0700

    Commented out 20k and 100k tests. They work locally. No need to stall CI with them.

--- test/unit/test_parser.py
+++ test/unit/test_parser.py
@@ -39,36 +39,36 @@ def test_parse_terms_1k():
     assert expected_string.strip() == term_string.strip()
 
 
-def test_parse_terms_20k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+# def test_parse_terms_20k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/20k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     term_string = parser.parse_terms(data_string)
+#
+#     file = os.path.join(dir_path, "../data/terms_expected_20k.txt")
+#     with open(file, "r") as expected:
+#         expected_string = expected.read()
+#
+#     assert expected_string.strip() == term_string.strip()
 
-    file = os.path.join(dir_path, "../data/20k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
 
-    term_string = parser.parse_terms(data_string)
-
-    file = os.path.join(dir_path, "../data/terms_expected_20k.txt")
-    with open(file, "r") as expected:
-        expected_string = expected.read()
-
-    assert expected_string.strip() == term_string.strip()
-
-
-def test_parse_terms_100k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    file = os.path.join(dir_path, "../data/100k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
-
-    term_string = parser.parse_terms(data_string)
-
-    file = os.path.join(dir_path, "../data/terms_expected_100k.txt")
-    with open(file, "r") as expected:
-        expected_string = expected.read()
-
-    assert expected_string.strip() == term_string.strip()
+# def test_parse_terms_100k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/100k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     term_string = parser.parse_terms(data_string)
+#
+#     file = os.path.join(dir_path, "../data/terms_expected_100k.txt")
+#     with open(file, "r") as expected:
+#         expected_string = expected.read()
+#
+#     assert expected_string.strip() == term_string.strip()
 
 
 def test_parse_pdates_10():
@@ -103,36 +103,36 @@ def test_parse_pdates_1k():
     assert expected_string.strip() == pdate_string.strip()
 
 
-def test_parse_pdates_20k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+# def test_parse_pdates_20k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/20k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     pdate_string = parser.parse_pdates(data_string)
+#
+#     file = os.path.join(dir_path, "../data/pdates_expected_20k.txt")
+#     with open(file, "r") as expected:
+#         expected_string = expected.read()
+#
+#     assert expected_string.strip() == pdate_string.strip()
 
-    file = os.path.join(dir_path, "../data/20k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
 
-    pdate_string = parser.parse_pdates(data_string)
-
-    file = os.path.join(dir_path, "../data/pdates_expected_20k.txt")
-    with open(file, "r") as expected:
-        expected_string = expected.read()
-
-    assert expected_string.strip() == pdate_string.strip()
-
-
-def test_parse_pdates_100k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    file = os.path.join(dir_path, "../data/100k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
-
-    pdate_string = parser.parse_pdates(data_string)
-
-    file = os.path.join(dir_path, "../data/pdates_expected_100k.txt")
-    with open(file, "r") as expected:
-        expected_string = expected.read()
-
-    assert expected_string.strip() == pdate_string.strip()
+# def test_parse_pdates_100k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/100k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     pdate_string = parser.parse_pdates(data_string)
+#
+#     file = os.path.join(dir_path, "../data/pdates_expected_100k.txt")
+#     with open(file, "r") as expected:
+#         expected_string = expected.read()
+#
+#     assert expected_string.strip() == pdate_string.strip()
 
 
 def test_parse_prices_10():
@@ -167,36 +167,36 @@ def test_parse_prices_1k():
     assert expected_string.strip() == price_string.strip()
 
 
-def test_parse_prices_20k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+# def test_parse_prices_20k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/20k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     price_string = parser.parse_prices(data_string)
+#
+#     file = os.path.join(dir_path, "../data/prices_expected_20k.txt")
+#     with open(file, "r") as expected:
+#         expected_string = expected.read()
+#
+#     assert expected_string.strip() == price_string.strip()
 
-    file = os.path.join(dir_path, "../data/20k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
 
-    price_string = parser.parse_prices(data_string)
-
-    file = os.path.join(dir_path, "../data/prices_expected_20k.txt")
-    with open(file, "r") as expected:
-        expected_string = expected.read()
-
-    assert expected_string.strip() == price_string.strip()
-
-
-def test_parse_prices_100k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    file = os.path.join(dir_path, "../data/100k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
-
-    price_string = parser.parse_prices(data_string)
-
-    file = os.path.join(dir_path, "../data/prices_expected_100k.txt")
-    with open(file, "r") as expected:
-        expected_string = expected.read()
-
-    assert expected_string.strip() == price_string.strip()
+# def test_parse_prices_100k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/100k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     price_string = parser.parse_prices(data_string)
+#
+#     file = os.path.join(dir_path, "../data/prices_expected_100k.txt")
+#     with open(file, "r") as expected:
+#         expected_string = expected.read()
+#
+#     assert expected_string.strip() == price_string.strip()
 
 
 def test_parse_ads_10():
@@ -231,33 +231,33 @@ def test_parse_ads_1000():
     assert expected_string.strip() == ad_string.strip()
 
 
-def test_parse_ads_20k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+# def test_parse_ads_20k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/20k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     ad_string = parser.parse_ads(data_string)
+#
+#     file = os.path.join(dir_path, "../data/ads_expected_20k.txt")
+#     with open(file, "r") as data:
+#         expected_string = data.read()
+#
+#     assert expected_string.strip() == ad_string.strip()
 
-    file = os.path.join(dir_path, "../data/20k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
 
-    ad_string = parser.parse_ads(data_string)
-
-    file = os.path.join(dir_path, "../data/ads_expected_20k.txt")
-    with open(file, "r") as data:
-        expected_string = data.read()
-
-    assert expected_string.strip() == ad_string.strip()
-
-
-def test_parse_ads_100k():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-
-    file = os.path.join(dir_path, "../data/100k.xml")
-    with open(file, "r") as data:
-        data_string = data.read()
-
-    ad_string = parser.parse_ads(data_string)
-
-    file = os.path.join(dir_path, "../data/ads_expected_100k.txt")
-    with open(file, "r") as data:
-        expected_string = data.read()
-
-    assert expected_string.strip() == ad_string.strip()
+# def test_parse_ads_100k():
+#     dir_path = os.path.dirname(os.path.realpath(__file__))
+#
+#     file = os.path.join(dir_path, "../data/100k.xml")
+#     with open(file, "r") as data:
+#         data_string = data.read()
+#
+#     ad_string = parser.parse_ads(data_string)
+#
+#     file = os.path.join(dir_path, "../data/ads_expected_100k.txt")
+#     with open(file, "r") as data:
+#         expected_string = data.read()
+#
+#     assert expected_string.strip() == ad_string.strip()
