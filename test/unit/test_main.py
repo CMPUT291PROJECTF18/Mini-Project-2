8d2690ce1a2351d65b8053bbf03e3f68c3363cd4
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-25 18:46:07 -0700

    adding some shotgun tests

--- test/unit/test_main.py
+++ test/unit/test_main.py
@@ -3,9 +3,44 @@
 
 """pytests for :mod:`.__main__`"""
 
-from mini_project_2.__main__ import get_parser
+import pytest
+
+from mini_project_2.__main__ import get_parser, main
 
 
 def test_get_parser():
     parser = get_parser()
     assert parser
+
+
+@pytest.mark.parametrize("cmd",
+    [
+        "price>-30 term=camera".split(),
+        "price>0 term=camera".split(),
+        "price<30 term=camera".split(),
+        "price=30".split(),
+        "price>=30".split(),
+        "price<=30".split(),
+        "price<30".split(),
+        "price>30".split(),
+        "term=camera".split(),
+        "term=camera%".split(),
+        "location=Red-deer".split(),
+        "location=Calgary".split(),
+        "price>30 term=camera location=Calgary".split(),
+        "price>30 term=camera location=Red-Deer".split(),
+        "price>30 term=camera location=Red-Deer cat=nonsuchcat".split(),
+        "cat=nonsuchcat".split(),
+        "date=2018/01/01".split(),
+        "date>=2018/01/01".split(),
+        "date<=2018/01/01".split(),
+        "date>2018/01/01".split(),
+        "date<2018/01/01".split(),
+        "price>30 term=camera location=Red-Deer cat=nonsuchcat date>2018/01/01".split(),
+    ]
+)
+def test_main(cmd):
+    """Bad tests to ensure nothing is extremely broken"""
+    base_cmd = """-ad mini_project_2/scripts/data/indexes/ads.idx -te mini_project_2/scripts/data/indexes/terms.idx -da mini_project_2/scripts/data/indexes/pdates.idx -pr mini_project_2/scripts/data/indexes/prices.idx -o full -v --log-level INFO""".split()
+    main(base_cmd+cmd)
+    # TODO: more better testing.
