bcef3fed6e05ee4c595399452376494a2f754dde
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-25 18:58:07 -0700

    improving checks for `term_alpha_numeric`

--- test/unit/test_main.py
+++ test/unit/test_main.py
@@ -6,7 +6,8 @@ import argparse
 
 import pytest
 
-from mini_project_2.__main__ import get_parser, main, alpha_numeric
+from mini_project_2.__main__ import get_parser, main, alpha_numeric, \
+    term_alpha_numeric
 
 
 def test_get_parser():
@@ -15,70 +16,119 @@ def test_get_parser():
 
 
 @pytest.mark.parametrize("cmd",
-    [
-        "price>-30 term=camera".split(),
-        "price>0 term=camera".split(),
-        "price<30 term=camera".split(),
-        "price=30".split(),
-        "price>=30".split(),
-        "price<=30".split(),
-        "price<30".split(),
-        "price>30".split(),
-        "term=camera".split(),
-        "term=camera%".split(),
-        "location=Red-deer".split(),
-        "location=Calgary".split(),
-        "price>30 term=camera location=Calgary".split(),
-        "price>30 term=camera location=Red-Deer".split(),
-        "price>30 term=camera location=Red-Deer cat=nonsuchcat".split(),
-        "cat=nonsuchcat".split(),
-        "date=2018/01/01".split(),
-        "date>=2018/01/01".split(),
-        "date<=2018/01/01".split(),
-        "date>2018/01/01".split(),
-        "date<2018/01/01".split(),
-        "price>30 term=camera location=Red-Deer cat=nonsuchcat date>2018/01/01".split(),
-    ]
-)
+                         [
+                             "price>-30 term=camera".split(),
+                             "price>0 term=camera".split(),
+                             "price<30 term=camera".split(),
+                             "price=30".split(),
+                             "price>=30".split(),
+                             "price<=30".split(),
+                             "price<30".split(),
+                             "price>30".split(),
+                             "term=camera".split(),
+                             "term=camera%".split(),
+                             "location=Red-deer".split(),
+                             "location=Calgary".split(),
+                             "price>30 term=camera location=Calgary".split(),
+                             "price>30 term=camera location=Red-Deer".split(),
+                             "price>30 term=camera location=Red-Deer cat=nonsuchcat".split(),
+                             "cat=nonsuchcat".split(),
+                             "date=2018/01/01".split(),
+                             "date>=2018/01/01".split(),
+                             "date<=2018/01/01".split(),
+                             "date>2018/01/01".split(),
+                             "date<2018/01/01".split(),
+                             "price>30 term=camera location=Red-Deer cat=nonsuchcat date>2018/01/01".split(),
+                         ]
+                         )
 def test_main(cmd):
     """Bad tests to ensure nothing is extremely broken"""
     base_cmd = """-ad mini_project_2/scripts/data/indexes/ads.idx -te mini_project_2/scripts/data/indexes/terms.idx -da mini_project_2/scripts/data/indexes/pdates.idx -pr mini_project_2/scripts/data/indexes/prices.idx -o full -v --log-level INFO""".split()
-    main(base_cmd+cmd)
+    main(base_cmd + cmd)
     # TODO: more better testing.
 
 
 @pytest.mark.parametrize("alpha_numeric_str",
-    [
-        "a",
-        "z"
-        "1",
-        "A",
-        "Z",
-        "-",
-        "_",
-        "ABC123abc-_",
-        "A-b"
-    ]
-)
+                         [
+                             "a",
+                             "z"
+                             "1",
+                             "A",
+                             "Z",
+                             "-",
+                             "_",
+                             "ABC123abc-_",
+                             "A-b"
+                         ]
+                         )
 def test_alpha_numeric(alpha_numeric_str):
     assert alpha_numeric(alpha_numeric_str)
 
 
 @pytest.mark.parametrize("non_alpha_numeric_str",
-    [
-        "",
-        "Ð"
-        "*",
-        "{}}",
-        "a*"
-        "b???/",
-        "125$$@#22",
-        "+‗",
-        "'",
-        "\\",
-        "//"
-    ]
-)
+                         [
+                             "",
+                             "Ð"
+                             "*",
+                             "{}}",
+                             "a*"
+                             "b???/",
+                             "125$$@#22",
+                             "+‗",
+                             "'",
+                             "\\",
+                             "//"
+                         ]
+                         )
 def test_alpha_numeric_fail(non_alpha_numeric_str):
     with pytest.raises(argparse.ArgumentTypeError):
         alpha_numeric(non_alpha_numeric_str)
+
+
+@pytest.mark.parametrize("term_alpha_numeric_str",
+                         [
+                             "a",
+                             "z"
+                             "1",
+                             "A",
+                             "Z",
+                             "-",
+                             "_",
+                             "ABC123abc-_",
+                             "A-b",
+                             "a%",
+                             "z%",
+                             "1%",
+                             "A%",
+                             "Z%",
+                             "-%",
+                             "_%",
+                             "ABC123abc-_%",
+                             "A-b%"
+                         ]
+                         )
+def test_term_alpha_numeric(term_alpha_numeric_str):
+    assert term_alpha_numeric(term_alpha_numeric_str)
+
+
+@pytest.mark.parametrize("term_alpha_numeric_str",
+                         [
+                             "",
+                             "Ð"
+                             "*",
+                             "{}}",
+                             "a*"
+                             "b???/",
+                             "125$$@#22",
+                             "+‗",
+                             "'",
+                             "\\",
+                             "//",
+                             "%1%",
+                             "_&&6%",
+                             "ABC1^^^23abc-_%",
+                         ]
+                         )
+def test_term_alpha_numeric_fail(term_alpha_numeric_str):
+    with pytest.raises(argparse.ArgumentTypeError):
+        assert term_alpha_numeric(term_alpha_numeric_str)
